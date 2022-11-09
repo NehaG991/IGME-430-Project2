008a1e012e13adc09c9bb407829072a48e1f9f76
Neha Ghanta <55118296+NehaG991@users.noreply.github.com>
2022-11-09 18:04:32 -0500

    implemented middleware

--- server/router.js
+++ server/router.js
@@ -1,17 +1,17 @@
 const controllers = require('./controllers');
-// const mid = require('./middleware');
+const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/', controllers.Account.loginPage);
+  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 
-  app.get('/login', controllers.Account.loginPage);
-  app.post('/login', controllers.Account.login);
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
-  app.post('/signup', controllers.Account.signup);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
-  app.get('/logout', controllers.Account.logout);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
-  app.get('/app', controllers.App.appPage);
+  app.get('/app', mid.requiresLogin, controllers.App.appPage);
 };
 
 module.exports = router;
